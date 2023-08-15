fdd7761dd449fae1a2cd8ebe1757085d811d70c7
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2023-08-15 13:27:42 +0000

    update paths to modules

--- tf/main.tf
+++ tf/main.tf
@@ -1,12 +1,12 @@
 
 module "api" {
-  source ="./api"
+  source ="./modules/api"
   project_ID = var.project_ID
   api_list =  var.api_list
 }
 
 module "gcs" {
-  source ="./cloud_storage"
+  source ="./modules/cloud_storage"
   project_ID = var.project_ID
   resource_location = var.resource_location
 }
