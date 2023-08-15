d87279e7790dd03c6491d79eabb4dfaf0ec7c8bd
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2023-08-15 12:39:09 +0000

    define terraform variables

---
+++ tf/var.auto.tfvars
@@ -0,0 +1,3 @@
+resource_location = "europe-central2"
+project_ID = "bqml-sandbox-396011"
+bq_data_location = "EU"
---
+++ tf/variables.tf
@@ -0,0 +1,17 @@
+variable resource_location {
+  type        = string
+  default     = "europe-central2"
+  description = "the GCP region in which the resources should be created"
+}
+
+variable project_ID {
+  type        = string
+  default     = ""
+  description = "the GCP project"
+}
+
+variable bq_data_location {
+  type        = string
+  default     = "UE"
+  description = "The location for BigQuery data"
+}
