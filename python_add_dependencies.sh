73280209dbfbb746527dcf5c4301c0c79d13d204
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2023-09-20 15:11:53 +0000

    add python dependecies file

---
+++ python_add_dependencies.sh
@@ -0,0 +1,25 @@
+# general dependencies
+poetry add pandas==2.0.*
+poetry add pyarrow==13.0.0
+poetry add db-dtypes==1.1.1
+poetry add numpy==1.24.*
+poetry add tqdm==4.66.*
+poetry add matplotlib==3.7.2
+poetry add ipywidgets==8.1.0
+poetry add ipykernel==6.24.0
+poetry add ipython==8.14.0
+poetry add jupyterlab==4.0.2
+
+# AI libraries
+poetry add scikit-learn==1.3.0
+poetry add tensorflow
+poetry add keras==2.13.1
+poetry add autokeras==1.1.0
+
+# GCP
+poetry add gcloud==0.18.3
+poetry add google-cloud-bigquery==3.4.1
+poetry add google-cloud-bigquery-storage==2.22.0
+poetry add google-cloud-storage==2.10.0
+
+
