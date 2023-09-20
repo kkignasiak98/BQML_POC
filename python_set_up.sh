8c104c344e2df839b7a669cd14e8a2a4e95035dd
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2023-09-20 15:09:34 +0000

    add file for python virtual env set-up

---
+++ python_set_up.sh
@@ -0,0 +1,13 @@
+pip install --upgrade pip # upgrade pip
+
+pip install poetry==1.6.1 # install library for creating and managing virtual envviroments
+
+poetry --version # check version
+
+poetry config virtualenvs.in-project true # make sure the virtualenv will be part of the project
+
+poetry init
+
+poetry install
+
+poetry env info
