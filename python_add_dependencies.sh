476e5a6250c4813c08030e55e0b2b98ece515f2f
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2023-09-24 08:12:32 +0000

    Reorganize dependencies file

--- python_add_dependencies.sh
+++ python_add_dependencies.sh
@@ -1,10 +1,3 @@
-# AI libraries
-poetry add scikit-learn==1.3.0
-#poetry add tensorflow
-#poetry add keras==2.13.1
-#poetry add autokeras 
-
-# autokeras will be added manually because tensorflow has some problem with poetry
 
 # general dependencies
 poetry add pandas==2.0.*
@@ -17,6 +10,15 @@ poetry add ipywidgets==8.1.0
 poetry add ipykernel==6.24.0
 poetry add ipython==8.14.0
 poetry add jupyterlab==4.0.2
+
+
+# AI libraries
+poetry add scikit-learn==1.3.0
+#poetry add tensorflow
+#poetry add keras==2.13.1
+#poetry add autokeras 
+
+# Keras and Tensorflow will be added manually because of the internal error.
 
 
 # GCP
